5c19c74307d2a1f09b5fec78b0caaf7a061586a4
ronson <chaufaitsing@gmail.com>
2023-06-04 23:57:09 +0800

    api change to http://localhost:3000/stockapi/ if in development

--- src/api/index.js
+++ src/api/index.js
@@ -3,16 +3,16 @@ import axios from "axios";
 // const proxyurl = "https://cors-anywhere.herokuapp.com/";
 // const proxyurl = "https://vue-http-demo-29666.web.app/";
 // const endpoint = "https://query1.finance.yahoo.com/";
-// const baseURL =
-//   process.env.NODE_ENV === "development"
-//     ? "http://localhost:8080/"
-//     : "https://ftrchau.github.io/stockapp-frontend-revamp/";
+const baseURL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/stockapi/"
+    : "https://stockapp-backend.vercel.app/stockapi/";
 
 const api = axios.create({
   //   baseURL: "https://query1.finance.yahoo.com/",
   // baseURL: baseURL,
   // baseURL: proxyurl + endpoint,
-  baseURL: "https://stockapp-backend-production.up.railway.app/stockapi/",
+  baseURL: baseURL,
   withCredentials: false,
   headers: {
     Accept: "application/json",
